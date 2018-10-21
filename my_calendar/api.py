b021d322101604b72688665f3bd95d639964725b
Jingxuan Zhu <zjxuan1996@outlook.com>
2018-10-21 15:46:25 -0500

    add type check on api

--- my_calendar/api.py
+++ my_calendar/api.py
@@ -3,7 +3,11 @@ from flask import session, g, request, Response
 from sqlalchemy import and_, extract
 from .database import db
 from .modules import User, Event, Tag
-from .utils import need_login, error_msg, json_response, check_fields
+from .utils import (
+    need_login, error_msg, json_response,
+    check_fields, check_email, check_word, check_datetime, check_exist_fields,
+    result_success, result_create_success
+)
 
 
 bp_api = Blueprint('api', __name__, url_prefix='/api')
@@ -20,25 +24,21 @@ def load_user():
 
 
 @bp_api.route('/users/login/', methods=['POST'])
-@check_fields(('email', str), ('password', str))
+@check_fields(('email', str, check_email), ('password', str, check_word))
 def login():
     content = request.json
     email, password = content['email'], content['password']
-    # input validation
     user = User.query.filter_by(email=email).first()
     if user is None:
         return error_msg(404, 'user not exist')
     if not user.verify(password):
         return error_msg(403, 'wrong password')
     session['user_id'] = user.user_id
-    return json_response({
-        'code': 200,
-        'msg': 'successfully login'
-    })
+    return result_success()
 
 
 @bp_api.route('/users/register/', methods=['POST'])
-@check_fields(('email', str), ('password', str))
+@check_fields(('email', str, check_email), ('password', str, check_word))
 def register():
     content = request.json
     email, password = content['email'], content['password']
@@ -48,25 +48,19 @@ def register():
     user = User.create(email, password)
     db.session.add(user)
     db.session.commit()
-    return json_response({
-        'code': 201,
-        'email': user.email
-    })
+    return result_create_success()
 
 
 @bp_api.route('/users/logout', methods=['GET'])
 @need_login
 def logout():
     session.clear()
-    return json_response({
-        'code': 200,
-        'msg': 'successfully logout'
-    })
+    return result_success()
 
 
 @bp_api.route('/events/user', methods=['POST'])
 @need_login
-@check_fields(('year', int), ('month', int))
+@check_fields(('year', int, lambda y: True), ('month', int, lambda m: 0 <= m <= 11))
 def get_user_events():
     content = request.json
     year, month = int(content['year']), int(content['month'])
@@ -82,36 +76,33 @@ def get_user_events():
 
 @bp_api.route('/events/create', methods=['POST'])
 @need_login
-@check_fields(('event_name', str), ('event_time', str))
+@check_fields(('event_name', str, check_word), ('event_time', str, check_datetime))
 def get_create():
     content = request.json
     event_name, event_time = content['event_name'], content['event_time']
     event = Event.create(event_name, event_time)
     g.user.events.append(event)
     db.session.commit()
-    return json_response({
-        'code': 201,
-        'msg': f'{event_name} create successfully'
-    })
+    return result_create_success()
 
 
 @bp_api.route('/events/update', methods=['POST'])
 @need_login
-@check_fields(('event_id', str), ('update_fields', dict))
+@check_fields(('event_id', str, check_word), ('update_fields', dict, check_exist_fields('event_name', 'event_time')))
 def update_events():
     content = request.json
     event_id, update_fields = content['event_id'], content['update_fields']
     event = Event.query.filter_by(event_id=event_id).first()
     if 'event_name' in update_fields:
-        event.event_name = update_fields['event_name']
-    elif 'event_time' in update_fields:
-        event.event_name = update_fields['event_name']
-    else:
-        return error_msg(403, 'fields not complete')
+        event_name = update_fields['event_name']
+        if check_word(event_name):
+            event.event_name = event_name
+    if 'event_time' in update_fields:
+        event_time = update_fields['event_time']
+        if check_datetime(event_time):
+            event.event_time = event_time
     db.session.commit()
-    return json_response({
-        'code': 200,
-    })
+    return result_success()
 
 
 @bp_api.route('/events/delete', methods=['POST'])
@@ -123,6 +114,4 @@ def delete_event():
     event = Event.query.filter_by(event_id=event_id).first()
     db.session.delete(event)
     db.session.commit()
-    return json_response({
-        'code': 200
-    })
+    return result_success()
