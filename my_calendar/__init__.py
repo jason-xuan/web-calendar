22241b10a772f82d73c7387657a4b3a80249b43b
jason zhu <zjxuan1996@outlook.com>
2018-10-22 12:17:24 -0500

    http only

--- my_calendar/__init__.py
+++ my_calendar/__init__.py
@@ -7,7 +7,8 @@ config = {
     'SESSION_TYPE': 'memcached',
     'SQLALCHEMY_DATABASE_URI':
         'mysql+mysqlconnector://wustl_inst:wustl_pass@ec2-52-14-93-16.us-east-2.compute.amazonaws.com:3306/orm_calendar',
-    'SQLALCHEMY_TRACK_MODIFICATIONS': True
+    'SQLALCHEMY_TRACK_MODIFICATIONS': True,
+    'SESSION_COOKIE_HTTPONLY': True,
 }
 
 
