f069fc5b6a7a4ce8bb8ff630d069e34af978182f
Jingxuan Zhu <zjxuan1996@outlook.com>
2018-10-24 00:45:41 -0500

    break tag into parts

--- app.py
+++ app.py
@@ -1,21 +1,8 @@
 from my_calendar import create_app
 
 
+app = create_app()
+
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
-
-
-# test code
-# from my_calendar import create_app
-# from my_calendar.database import db
-# from my_calendar.modules import User, Event, Tag
-# from datetime import datetime
-# app = create_app()
-# app.app_context().push()
-#
-# user = User.create('jason@xua', '12345')
-# db.session.add(user)
-# db.session.commit()
-# event = Event.create('dinner', datetime(2018, 9, 10))
-# event = Event.create('dinner', datetime(2018, 9, 11))
